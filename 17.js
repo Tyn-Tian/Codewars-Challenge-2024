257894addd4d2d19987739d84c467975f65a1a60
lukxnut <kristian2015ok@gmail.com>
2024-02-15 19:31:33 +0700

    Update documentation and challenge 17/365

---
+++ 17.js
@@ -0,0 +1,30 @@
+function likes(names) {
+    switch (names.length >= 0) {
+        case names.length <= 1:
+            return `${names.length != 0 ? names : 'no one'} likes this`
+        case names.length >= 4:
+            const twoFirstName = names.slice(0, 2)
+            return `${twoFirstName.join(', ')} and ${names.length - 2} others like this`
+        default:
+            const lastName = names.pop()
+            return `${names.join(', ')} and ${lastName} like this`
+    }
+}
+
+// Top Answer sort by Best Practice
+// function likes(names) {
+//     names = names || [];
+//     switch (names.length) {
+//         case 0: return 'no one likes this';
+//         case 1: return names[0] + ' likes this';
+//         case 2: return names[0] + ' and ' + names[1] + ' like this';
+//         case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+//         default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+//     }
+// }
+
+console.log(likes([]));
+console.log(likes(["Peters"]));
+console.log(likes(["Peters", 'Jacob']));
+console.log(likes(["Peters", 'Jacob', 'Mark']));
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
--- readme.md
+++ readme.md
@@ -274,3 +274,25 @@ There will always be only one integer that appears an odd number of times.
 - Tags:
   - Fundamentals
 - Link: [Find the odd int](https://www.codewars.com/kata/54da5a58ea159efa38000836)
+
+### 17. Who likes it?
+
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+```javascript
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+```
+
+Note: For 4 or more names, the number in **"and 2 others"** simply increases.
+
+- Difficulty: _6 kyu_
+- Tags
+  - Strings
+  - Fundamentals
+- Link: [Who likes it?](https://www.codewars.com/kata/5266876b8f4bf2da9b000362)
