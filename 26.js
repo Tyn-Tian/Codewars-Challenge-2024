c1a2518017afcf17195381309da01b81fc59311c
lukxnut <kristian2015ok@gmail.com>
2024-03-03 15:37:57 +0700

    Update documentation and challenge 26/365

---
+++ 26.js
@@ -0,0 +1,15 @@
+function isSquare(n) {
+    return Number.isInteger(Math.sqrt(n))
+}
+
+// Top answer sort by best practices
+// function isSquare(n) {
+//     return Math.sqrt(n) % 1 === 0;
+// }
+
+console.log(isSquare(-1))
+console.log(isSquare(0))
+console.log(isSquare(3))
+console.log(isSquare(4))
+console.log(isSquare(25))
+console.log(isSquare(26))
