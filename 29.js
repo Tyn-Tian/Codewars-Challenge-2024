09a62721ede2f0160bdcf4ffa8da31c130403e22
lukxnut <kristian2015ok@gmail.com>
2024-03-04 20:11:47 +0700

    Update documentation and challenge 29/365

---
+++ 29.js
@@ -0,0 +1,13 @@
+function findShort(strings) {
+    const wordLength = strings.split(' ').map(n => n.length)
+    return Math.min(...wordLength)
+}
+
+// Top answer sort by best practices
+// function findShort(s){
+//     return Math.min(...s.split(" ").map (s => s.length));
+// }
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(findShort("turns out random test cases are easier than writing out basic ones"))
+console.log(findShort("Let's travel abroad shall we"))
--- readme.md
+++ readme.md
@@ -534,3 +534,14 @@ XO("zzoo") => false
 - Tags:
   - Fundamentals
 - Link: [Exes and Ohs](https://www.codewars.com/kata/55908aad6620c066bc00002a)
+
+### 29. Shortest Word
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+- Difficulty: _7 kyu_
+- Tags:
+  - Fundamentals
+- Link: [Shortest Word](https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9)
